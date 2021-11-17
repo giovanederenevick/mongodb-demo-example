5c72c9aa7bb10450d06a715dcf5082a76942f230
Giovane Derenevick Filho <giovanedfilho@gmail.com>
2021-11-17 09:02:02 -0300

    Project created

---
+++ src/test/java/br/com/giovanederenevick/mongodbdemo/MongodbDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.giovanederenevick.mongodbdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongodbDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
